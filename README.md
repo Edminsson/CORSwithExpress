4d880e045608f78863388ccb1b26a7e1d07cd9f1
Axel Aguilar <abc709@hotmail.com>
2016-09-24 17:17:27 +0200

    Making sure CORS for token based authentication works

--- README.md
+++ README.md
@@ -4,3 +4,36 @@ Simple Express site that sets some headers required for CORS to work.
 Access-Control-Allow-Origin can be set to "*" in some simple cases but when withCredentials is 
 used it must be the same as the requests origin header.
 Also Access-Control-Allow-Credentials must be se to true.
+
+When using token based authentication preflighted CORS must be handled.
+That means responding to the OPTIONS request and making sure that Authorization is
+in the Access-Control-Allow-Headers.  
+
+##Ajax-anrop med withCredentials
+
+var settings = {
+    url: 'http://localhost:5000/data/animal.json',
+    type: 'GET',
+    dataType: 'json',
+    xhrFields: { withCredentials: true },
+    success: (json) => {
+        console.log('OK', json);
+    }
+};
+$.ajax(settings);
+
+##Ajax-anrop med token-based-authentication
+
+var settings = {
+    url: 'http://localhost:5000/data/animal.json',
+    type: 'GET',
+    dataType: 'json',
+    beforeSend: (xhr) => {
+        xhr.setRequestHeader("Authorization", "Bearer " + "thisIsTheToken");
+    },
+    success: (json) => {
+        console.log('OK', json);
+    }
+};
+$.ajax(settings);
+
--- app.js
+++ app.js
@@ -4,12 +4,28 @@ var app = express();
 var port = 5000;
 
 app.use(function(req, res, next) {
+    if (req.method === 'OPTIONS') {
+        console.log('!OPTIONS');
+        var headers = {};
+        // IE8 does not allow domains to be specified, just the *
+        // headers["Access-Control-Allow-Origin"] = req.headers.origin;
+        headers["Access-Control-Allow-Origin"] = "*";
+        headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+        headers["Access-Control-Allow-Credentials"] = false;
+        headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+        headers["Access-Control-Allow-Headers"] = "Authorization, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+        res.writeHead(200, headers);
+        res.end();
+    }
+
     var origin = req.headers.origin ? req.headers.origin : "*";
     //origin cannot be * when using withCredentials for CORS to work
     res.header("Access-Control-Allow-Origin", origin);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     //Access-Control-Allow-Credentials is required for CORS to work with withCredentials
     res.header("Access-Control-Allow-Credentials", true)
+
+
     next();
 });
 
